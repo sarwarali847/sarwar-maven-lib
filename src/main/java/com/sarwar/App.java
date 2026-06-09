a30ba340890a12bcb030f932d5eb51e1a6c5895d
sarwarali847 <sarwarali847@gmail.com>
2026-06-09 23:30:13 +0530

    Initial Maven package

---
+++ src/main/java/com/sarwar/App.java
@@ -0,0 +1,16 @@
+package com.sarwar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello from Sarwar Maven Package");
+    }
+
+    public static String getMessage() {
+        return "Hello from Sarwar Maven Package";
+    }
+}
